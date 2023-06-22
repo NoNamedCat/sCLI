245c05205a4ff5dbdb7a33c8ab97b12dfc36cfe5
NoNamedCat <39467683+NoNamedCat@users.noreply.github.com>
2023-06-22 03:32:34 -0500

    Update calc.ino

--- examples/calculator/calc.ino
+++ examples/calculator/calc.ino
@@ -1,140 +1,152 @@
-#include "sCLI.h"
+#include <Arduino.h>
+#include <sCLI.h>
 #include <math.h>
 
 sCLI cli;
 
-// Function to calculate the square root of a number
-void calculateSqrt(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 1) {
-    float number = parameters[0].toFloat();
-    float result = sqrt(number);
-    stream.print("Square root of ");
-    stream.print(number);
-    stream.print(" is: ");
-    stream.println(result);
-  } else {
-    stream.println("Incorrect number of parameters. Usage: sqrt [number]");
-  }
-}
-
-// Function to calculate the cube root of a number
-void calculateCbrt(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 1) {
-    float number = parameters[0].toFloat();
-    float result = cbrt(number);
-    stream.print("Cube root of ");
-    stream.print(number);
-    stream.print(" is: ");
-    stream.println(result);
-  } else {
-    stream.println("Incorrect number of parameters. Usage: cbrt [number]");
-  }
-}
-
-// Function to calculate the power of a number
-void calculatePower(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 2) {
-    float base = parameters[0].toFloat();
-    float exponent = parameters[1].toFloat();
-    float result = pow(base, exponent);
-    stream.print(base);
-    stream.print(" raised to the power of ");
-    stream.print(exponent);
-    stream.print(" is: ");
-    stream.println(result);
-  } else {
-    stream.println("Incorrect number of parameters. Usage: power [base] [exponent]");
-  }
-}
-
-// Function to calculate the logarithm of a number
-void calculateLog(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 2) {
-    float number = parameters[0].toFloat();
-    float base = parameters[1].toFloat();
-    float result = log(number) / log(base);
-    stream.print("Logarithm of ");
-    stream.print(number);
-    stream.print(" base ");
-    stream.print(base);
-    stream.print(" is: ");
-    stream.println(result);
-  } else {
-    stream.println("Incorrect number of parameters. Usage: log [number] [base]");
-  }
-}
-
-// Function to perform addition
-void performAddition(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 2) {
-    float num1 = parameters[0].toFloat();
-    float num2 = parameters[1].toFloat();
-    float result = num1 + num2;
-    stream.print("Result of addition: ");
-    stream.println(result);
-  } else {
-    stream.println("Incorrect number of parameters. Usage: add [num1] [num2]");
-  }
-}
-
-// Function to perform subtraction
-void performSubtraction(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 2) {
-    float num1 = parameters[0].toFloat();
-    float num2 = parameters[1].toFloat();
-    float result = num1 - num2;
-    stream.print("Result of subtraction: ");
-    stream.println(result);
-  } else {
-    stream.println("Incorrect number of parameters. Usage: subtract [num1] [num2]");
-  }
-}
-
-// Function to perform multiplication
-void performMultiplication(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 2) {
-    float num1 = parameters[0].toFloat();
-    float num2 = parameters[1].toFloat();
-    float result = num1 * num2;
-    stream.print("Result of multiplication: ");
-    stream.println(result);
-  } else {
-    stream.println("Incorrect number of parameters. Usage: multiply [num1] [num2]");
-  }
-}
-
-// Function to perform division
-void performDivision(Stream& stream, const String parameters[], byte numParams) {
-  if (numParams == 2) {
-    float num1 = parameters[0].toFloat();
-    float num2 = parameters[1].toFloat();
-    if (num2 != 0) {
-      float result = num1 / num2;
-      stream.print("Result of division: ");
-      stream.println(result);
-    } else {
-      stream.println("Cannot divide by zero.");
-    }
-  } else {
-    stream.println("Incorrect number of parameters. Usage: divide [num1] [num2]");
-  }
-}
-
 void setup() {
   Serial.begin(9600);
-  cli.addPort(Serial);
+  cli.addStream(Serial);
+  cli.setPrompt("sCLI> ");
 
-  // Add commands to the CLI
-  cli.addCommand("sqrt", calculateSqrt, 1);
-  cli.addCommand("cbrt", calculateCbrt, 1);
-  cli.addCommand("power", calculatePower, 2);
-  cli.addCommand("log", calculateLog, 2);
-  cli.addCommand("add", performAddition, 2);
-  cli.addCommand("subtract", performSubtraction, 2);
-  cli.addCommand("multiply", performMultiplication, 2);
-  cli.addCommand("divide", performDivision, 2);
+  // Add command functions
+  cli.addCommand("add", add);
+  cli.addCommand("subtract", subtract);
+  cli.addCommand("multiply", multiply);
+  cli.addCommand("divide", divide);
+  cli.addCommand("sqrt", squareRoot);
+  cli.addCommand("power", power);
+  cli.addCommand("log", logarithm);
+
+  // Start sCLI
+  cli.loop();
 }
 
 void loop() {
-  cli.loop();
+  // Empty loop
 }
+
+// Command Functions
+
+void add(const char** params, uint8_t paramCount) {
+  if (paramCount != 2) {
+    cli.print("Usage: add <num1> <num2>\n");
+    return;
+  }
+
+  float num1 = atof(params[1]);
+  float num2 = atof(params[2]);
+  float result = num1 + num2;
+
+  cli.print("Result: ");
+  cli.printNumber(result);
+  cli.print("\n");
+}
+
+void subtract(const char** params, uint8_t paramCount) {
+  if (paramCount != 2) {
+    cli.print("Usage: subtract <num1> <num2>\n");
+    return;
+  }
+
+  float num1 = atof(params[1]);
+  float num2 = atof(params[2]);
+  float result = num1 - num2;
+
+  cli.print("Result: ");
+  cli.printNumber(result);
+  cli.print("\n");
+}
+
+void multiply(const char** params, uint8_t paramCount) {
+  if (paramCount != 2) {
+    cli.print("Usage: multiply <num1> <num2>\n");
+    return;
+  }
+
+  float num1 = atof(params[1]);
+  float num2 = atof(params[2]);
+  float result = num1 * num2;
+
+  cli.print("Result: ");
+  cli.printNumber(result);
+  cli.print("\n");
+}
+
+void divide(const char** params, uint8_t paramCount) {
+  if (paramCount != 2) {
+    cli.print("Usage: divide <num1> <num2>\n");
+    return;
+  }
+
+  float num1 = atof(params[1]);
+  float num2 = atof(params[2]);
+
+  if (num2 == 0) {
+    cli.print("Error: Division by zero\n");
+    return;
+  }
+
+  float result = num1 / num2;
+
+  cli.print("Result: ");
+  cli.printNumber(result);
+  cli.print("\n");
+}
+
+void squareRoot(const char** params, uint8_t paramCount) {
+  if (paramCount != 1) {
+    cli.print("Usage: sqrt <num>\n");
+    return;
+  }
+
+  float num = atof(params[1]);
+
+  if (num < 0) {
+    cli.print("Error: Invalid input\n");
+    return;
+  }
+
+  float result = sqrt(num);
+
+  cli.print("Result: ");
+  cli.printNumber(result);
+  cli.print("\n");
+}
+
+void power(const char** params, uint8_t paramCount) {
+  if (paramCount != 2) {
+    cli.print("Usage: power <base> <exponent>\n");
+    return;
+  }
+
+  float base = atof(params[1]);
+  float exponent = atof(params[2]);
+  float result = pow(base, exponent);
+
+  cli.print("Result: ");
+  cli.printNumber(result);
+  cli.print("\n");
+}
+
+void logarithm(const char** params, uint8_t paramCount) {
+  if (paramCount != 2) {
+    cli.print("Usage: log <base> <number>\n");
+    return;
+  }
+
+  float base = atof(params[1]);
+  float number = atof(params[2]);
+
+  if (base <= 0 || number <= 0) {
+    cli.print("Error: Invalid input\n");
+    return;
+  }
+
+  float result = log(number) / log(base);
+
+  cli.print("Result: ");
+  cli.printNumber(result);
+  cli.print("\n");
+}
+
