8d2d7b2880616ea05168ae2b910b0d06462ebc64
NoNamedCat <39467683+NoNamedCat@users.noreply.github.com>
2023-06-22 03:33:16 -0500

    Update sCLI.h

--- src/sCLI.h
+++ src/sCLI.h
@@ -5,35 +5,37 @@
 #include <Stream.h>
 
 #define MAX_COMMAND_LENGTH 64
-#define MAX_PARAMETERS 5
-#define MAX_PORTS 5
-#define MAX_COMMANDS 10
+#define MAX_PARAM_COUNT 8
 
-typedef void (*CommandFunction)(Stream& stream, const String parameters[], byte numParams);
+typedef void (*CommandFunction)(const char** params, uint8_t paramCount);
 
 class sCLI {
-public:
-  sCLI();
-  void addCommand(const String& command, CommandFunction function, byte numParams);
-  void addPort(Stream& stream);
-  void print(const String& message);
-  void loop();
+  public:
+    sCLI();
+    void addCommand(const char* command, CommandFunction function);
+    void addStream(Stream& stream);
+    void setPrompt(const char* prompt);
+    void print(const char* message);
+    void println(const char* message);
+    void printNumber(int number);
+    void printNumber(float number, uint8_t decimalPlaces);
+    void loop();
 
-private:
-  Stream* ports[MAX_PORTS];
-  byte numPorts;
-  String inputBuffer;
-  byte inputCount;
-  CommandFunction commands[MAX_COMMANDS];
-  String commandNames[MAX_COMMANDS];
-  byte numCommands;
-  String parameters[MAX_PARAMETERS];
-  byte numParams;
+  private:
+    char commandBuffer[MAX_COMMAND_LENGTH];
+    const char* prompt;
+    CommandFunction commandFunctions[MAX_COMMAND_LENGTH];
+    const char* commandNames[MAX_COMMAND_LENGTH];
+    uint8_t commandCount;
+    Stream* streams[MAX_COMMAND_LENGTH];
+    uint8_t streamCount;
+    uint8_t charCount;
 
-  void processCommand();
-  void clearInput();
-  void printPrompt();
+    void executeCommand();
+    void processInput();
+    void printPrompt();
+    
+    void tokenizeCommand(const char* command, char** tokens, uint8_t& tokenCount);
 };
 
 #endif
-
